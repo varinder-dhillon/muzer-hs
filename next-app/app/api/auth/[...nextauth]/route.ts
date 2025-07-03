4a636ce09f4b2d8bc9a20455b915a3a14543ccd7
Varinder <varinderdhilloninfo@gmail.com>
2025-07-03 23:23:46 +0530

    Feat (Auth): Added callbacks for signin method

--- next-app/app/api/auth/[...nextauth]/route.ts
+++ next-app/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,32 @@
+import { prismaClient } from "@/lib/db/db";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 // import { NextResponse } from "next/server";
 
 const handler = NextAuth({
     providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-    })
-    ]
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        })
+    ],
+    callbacks: {
+        async signIn (params) {
+            if(!params.user.email) return false;
+
+            try {
+                await prismaClient.user.create({
+                    data: {
+                        email: params.user.email,
+                        provider: "Google"
+                    }
+                })
+            } catch (error) {
+                return false
+            }
+            return true
+        }
+    }
 })
 
 // const handler = () => {
